d050414d75fed8099ea0ae87586442e974e70fbc
G3MB3L INT3RN3T <gembelinternet404@gmail.com>
2021-06-07 03:32:33 +0700

    initialize dotenv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -34,4 +36,4 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-app.listen(3000);
+app.listen(process.env.HTTP_PORT || 3000);
