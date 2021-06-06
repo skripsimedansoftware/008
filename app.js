9f3d7d9cd2ab139c0eb10087d1d7e13e691915a6
G3MB3L INT3RN3T <gembelinternet404@gmail.com>
2021-06-07 03:05:39 +0700

    Modify load routers

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require(__dirname+'/routes/index'));
+app.use('/users', require(__dirname+'/routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
