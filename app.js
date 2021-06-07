8d3ba602c863a659e21b31c30772bf1562d4b7ba
G3MB3L INT3RN3T <gembelinternet404@gmail.com>
2021-06-07 09:42:29 +0700

    add & initialize package cors, async, mongodb

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const async = require('async');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session')
@@ -13,6 +15,37 @@ Models = require(__dirname+'/app/models');
 Helpers = require(__dirname+'/app/helpers');
 Libraries = require(__dirname+'/app/libraries');
 Middlewares = require(__dirname+'/app/middlewares');
+DB = {};
+
+async.waterfall([
+	/**
+	 * Initialize database
+	 */
+	async function initializeDatabase(callback) {
+		var ACTIVE_DATABASE = Config.database[process.env.ACTIVE_DATABASE];
+		if (ACTIVE_DATABASE.dbdriver.toLowerCase().match(/(mongo|mongodb)/)) {
+			const MongoDB = require('mongodb').MongoClient;
+			const DBConfig = new MongoDB('mongodb://'+ACTIVE_DATABASE.host+':'+ACTIVE_DATABASE.port, { useUnifiedTopology: true });
+			app.set('database', {
+				database: ACTIVE_DATABASE.database,
+				connection: await DBConfig.connect()
+			});
+		}
+	},
+
+	/**
+	 * Activate database
+	 */
+	function activateDatabase(error, callback) {
+		Object.assign(DB, app.get('database').connection.db(app.get('database').database));
+		callback(null, callback);
+	}
+], (error) => {
+	if (error) {
+		console.log(error);
+		process.exit(0);
+	}
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +60,7 @@ app.use(session({
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin : (origin, callback) => { callback(null, true) }, credentials: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
