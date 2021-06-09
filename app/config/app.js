cf956b62b5ba22a0a89947dcaece028f61bffd23
G3MB3L INT3RN3T <gembelinternet404@gmail.com>
2021-06-10 01:30:29 +0700

    added app config

---
+++ app/config/app.js
@@ -0,0 +1,9 @@
+/**
+ * Application name
+ */
+module.exports.name  = (process.env.APP_NAME !== undefined)?process.env.APP_NAME:'ExpressJs Starter';
+
+/**
+ * Application vendor
+ */
+module.exports.vendor = (process.env.APP_VENDOR !== undefined)?process.env.APP_VENDOR:'MedanSoftware';
