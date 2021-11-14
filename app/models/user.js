08997367b418b2d1bee94b8a02ee949020c33bcd
agoenks29D <agungmasda29@gmail.com>
2021-11-14 13:41:05 +0700

    change define field type

--- app/models/user.js
+++ app/models/user.js
@@ -2,21 +2,21 @@ module.exports = {
 	connections: [process.env.ACTIVE_DATABASE],
 	fields: {
 		id: {
-			type: Libraries.sequelize.DataTypes.BIGINT.UNSIGNED,
+			type: DB.DataTypes.BIGINT.UNSIGNED,
 			primaryKey: true,
 			autoIncrement: true
 		},
 		email: {
-			type: Libraries.sequelize.DataTypes.STRING(80)
+			type: DB.DataTypes.STRING(80)
 		},
 		username: {
-			type: Libraries.sequelize.DataTypes.STRING(16)
+			type: DB.DataTypes.STRING(16)
 		},
 		password: {
-			type: Libraries.sequelize.DataTypes.STRING(40)
+			type: DB.DataTypes.STRING(40)
 		},
 		full_name: {
-			type: Libraries.sequelize.DataTypes.STRING(60)
+			type: DB.DataTypes.STRING(60)
 		}
 	},
 	config: {}
