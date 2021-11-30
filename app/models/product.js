d456bcbbc037dc319d30b71e3bb18325841be9f7
Agung Dirgantara <agungmasda29@gmail.com>
2021-12-01 04:32:13 +0700

    initialize product model

---
+++ app/models/product.js
@@ -0,0 +1,37 @@
+module.exports = function(DataTypes) {
+	return {
+	connections: [process.env.ACTIVE_DATABASE],
+		fields: {
+			id: {
+				type: DataTypes.BIGINT.UNSIGNED,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			sha1: {
+				type: DataTypes.STRING(40)
+			},
+			item_id: {
+				type: DataTypes.BIGINT.UNSIGNED
+			},
+			name: {
+				type: DataTypes.STRING(100)
+			},
+			real_price: {
+				type: DataTypes.DOUBLE
+			},
+			discount: {
+				type: DataTypes.DOUBLE
+			},
+			price_with_discount: {
+				type: DataTypes.DOUBLE
+			},
+			image: {
+				type: DataTypes.STRING(100)
+			},
+			date: {
+				type: DataTypes.DATEONLY
+			}
+		},
+		config: {}
+	}
+}
